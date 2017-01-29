56d188ccaed76f0275ea45cdece30124207adb3d
Yan Hong <onlyhongyan@gmail.com>
2017-01-28 16:49:55 -0800

    get student detail

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,25 @@
 var Student = React.createClass({
 
+    getInitialState: function() {
+        return {
+            student: {
+                firstname: this.props.student.firstname,
+                _id: this.props.student._id
+            }
+        }
+    },
+
     handleClick: function() {
-
-        console.log(this.props.student._id);
-
+        var ReactThis = this;
+        axios.get('http://localhost:3000/students/' + this.props.student._id)
+            .then(function(response) {
+                ReactThis.setState({
+                    student: response.data
+                })
+            })
+            .catch(function(err) {
+                console.log(err)
+            })
 
     },
 
@@ -11,7 +27,9 @@ var Student = React.createClass({
         return (
             <div>
                 <p onClick={this.handleClick}>{this.props.student.firstname}</p>
+                <p>{this.state.student.lastname}</p>
                 <p>{this.state.student.age}</p>
+                <p>{this.state.student.school}</p>
             </div>
         )
     }
